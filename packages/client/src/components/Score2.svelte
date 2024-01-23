72f64ba65d51d43ddb447d0ea8d8d8622486ef6d
Teemu Koivisto <tkoivisto456@gmail.com>
2024-01-23 22:35:22 +0200

    draw both target and played note

--- packages/client/src/components/Score2.svelte
+++ packages/client/src/components/Score2.svelte
@@ -2,15 +2,13 @@
   import { played, target } from '$stores/game'
   import Vex from 'vexflow'
 
-  const { Factory, EasyScore, System, Renderer, Stave } = Vex.Flow
+  const { Factory, EasyScore, System, Renderer, Stave, StaveNote } = Vex.Flow
 
   import type { Note } from '@/types'
   import { onMount } from 'svelte'
 
-  let vf: Vex.Factory
-  let score: Vex.EasyScore
-  let system: Vex.System
   let outputEl: HTMLDivElement
+  let renderer: Vex.Renderer
 
   $: {
     if (typeof window !== undefined) {
@@ -23,43 +21,54 @@
   })
 
   function init() {
-    vf = new Factory({
-      renderer: { elementId: 'output', width: 500, height: 200 }
-    })
-
-    score = vf.EasyScore()
-    system = vf.System()
-    system
-      .addStave({
-        voices: [score.voice(score.notes('F#4/w', { stem: 'down' }))]
-      })
-      .addClef('treble')
-      .addTimeSignature('4/4')
-      .addKeySignature('Db')
-      .addClass('hidden')
-    vf.draw()
+    renderer = new Renderer(outputEl, Renderer.Backends.SVG)
+    renderer.resize(800, 300)
+    const context = renderer.getContext()
+    const s1 = new Stave(10, 40, 200)
+    s1.addClef('treble').addTimeSignature('4/4')
+    Vex.Flow.Formatter.FormatAndDraw(context, s1, [
+      new StaveNote({ keys: ['c/4'], duration: 'q' }),
+      new StaveNote({ keys: ['d/4'], duration: 'q' }),
+      new StaveNote({ keys: ['b/4'], duration: 'qr' }),
+      new StaveNote({ keys: ['c/4', 'e/4', 'g/4'], duration: 'q' })
+    ])
+    const s2 = new Stave(210, 40, 150)
+    Vex.Flow.Formatter.FormatAndDraw(context, s2, [
+      new StaveNote({ keys: ['c/4'], duration: 'q' }),
+      new StaveNote({ keys: ['d/4'], duration: 'q' }),
+      new StaveNote({ keys: ['b/4'], duration: 'qr' }),
+      new StaveNote({ keys: ['c/4', 'e/4', 'g/4'], duration: 'q' })
+    ])
+    s1.setContext(context).draw()
+    s2.setContext(context).draw()
   }
 
-  function updateNotes(target?: Note, played?: Note, correct?: boolean) {
-    const t = `${target ? target.absolute + '/w' : ''} ${played ? played.absolute + '/w' : ''}`
-    console.log('draw ', t)
-    system = vf.System()
-    const ctx = system.getContext()
-    ctx?.clear()
-    const voice = score.voice(
-      score.notes(t.replaceAll('♯', '#').replaceAll('♭', 'b'), { stem: 'up' })
-    )
-    let formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400)
-    system
-      .addStave({
-        voices: [voice]
+  function updateNotes(target: Note, played?: Note, correct?: boolean) {
+    const ctx = renderer.getContext()
+    ctx.clear()
+    const s1 = new Stave(10, 40, 200)
+    s1.addClef('treble').addTimeSignature('4/4')
+    const notes = [
+      new Vex.Flow.StaveNote({
+        clef: 'treble',
+        keys: [`${target.note}/${target.octave}`.replaceAll('♯', '#').replaceAll('♭', 'b')],
+        duration: 'h'
       })
-      .addClef('treble')
-      .addTimeSignature('4/4')
-      .addKeySignature('Db')
-      .format()
-
-    vf.draw()
+    ]
+    if (played) {
+      notes.push(
+        new Vex.Flow.StaveNote({
+          clef: 'treble',
+          keys: [`${played.note}/${played.octave}`.replaceAll('♯', '#').replaceAll('♭', 'b')],
+          duration: 'h'
+        })
+      )
+    } else {
+      notes.push(new StaveNote({ keys: ['g/4'], duration: 'hr' }))
+    }
+    Vex.Flow.Formatter.FormatAndDraw(ctx, s1, notes)
+    s1.setContext(ctx).draw()
+    // console.log('draw ', notes)
   }
 </script>
 
