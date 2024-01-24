40db739cda986c20507f6ce563ae3c5a2ebef0d8
Teemu Koivisto <tkoivisto456@gmail.com>
2024-01-24 13:43:49 +0200

    draw to staves without rests

--- packages/client/src/components/Score2.svelte
+++ packages/client/src/components/Score2.svelte
@@ -2,13 +2,15 @@
   import { played, target } from '$stores/game'
   import Vex from 'vexflow'
 
-  const { Accidental, Factory, EasyScore, System, Renderer, Stave, StaveNote } = Vex.Flow
+  const { Accidental, EasyScore, Factory, Formatter, System, Renderer, Stave, StaveNote } = Vex.Flow
 
   import type { Note } from '@/types'
   import { onMount } from 'svelte'
 
   let outputEl: HTMLDivElement
   let renderer: Vex.Renderer
+  let ctx: Vex.RenderContext
+  let tickContext: Vex.TickContext
 
   $: {
     if (typeof window !== undefined) {
@@ -23,7 +25,8 @@
   function init() {
     renderer = new Renderer(outputEl, Renderer.Backends.SVG)
     renderer.resize(732, 300)
-    const ctx = renderer.getContext()
+    ctx = renderer.getContext()
+    tickContext = new Vex.Flow.TickContext()
     const s1 = new Stave(10, 40, 200)
     s1.addClef('treble') //.addTimeSignature('4/4')
     const notes = [
@@ -45,51 +48,52 @@
     s2.setContext(ctx).draw()
   }
 
-  function createNote(note: Note): { sn: Vex.StaveNote; clef: 'treble' | 'bass' } {
+  function drawNote(
+    note: Note,
+    treble: Vex.Stave,
+    bass: Vex.Stave
+  ): { note: Vex.StemmableNote; clef: 'treble' | 'bass' } {
+    const clef = note.octave >= 4 ? 'treble' : 'bass'
     const sn = new Vex.Flow.StaveNote({
-      clef: note.octave >= 4 ? 'treble' : 'bass',
+      clef,
       keys: [`${note.parts[0]}${note.parts[1]}/${note.parts[2]}`],
-      duration: 'h'
+      duration: 'w'
     })
+    const stave = clef === 'treble' ? treble : bass
+    sn.setContext(ctx).setStave(stave)
     if (note.parts[1]) {
       sn.addModifier(new Accidental(note.parts[1]))
     }
-    return { sn, clef: note.octave >= 4 ? 'treble' : 'bass' }
+    tickContext.addTickable(sn)
+    // stave.draw()
+    // Formatter.FormatAndDraw(ctx, stave, [sn]);
+    return { note: sn, clef }
+  }
+
+  function drawNotesToStaves(
+    trebleStave: Vex.Stave,
+    bassStave: Vex.Stave,
+    notes: { note: Vex.StemmableNote; clef: 'treble' | 'bass' }[]
+  ) {
+    const trebleNotes = notes.filter(n => n.clef === 'treble').map(n => n.note)
+    const bassNotes = notes.filter(n => n.clef === 'bass').map(n => n.note)
+    if (trebleNotes.length > 0) {
+      Formatter.FormatAndDraw(ctx, trebleStave, trebleNotes)
+    }
+    if (bassNotes.length > 0) {
+      Formatter.FormatAndDraw(ctx, bassStave, bassNotes)
+    }
   }
 
   function updateNotes(target: Note, played?: Note, correct?: boolean) {
-    const ctx = renderer.getContext()
     ctx.clear()
-    const s1 = new Stave(10, 40, 200)
-    s1.addClef('treble') //.addTimeSignature('4/4')
-    const s2 = new Stave(10, 110, 200)
-    s2.addClef('bass')
-    const trebleNotes = []
-    const bassNotes = []
-    const t = createNote(target)
-    if (t.clef === 'treble') {
-      trebleNotes.push(t.sn)
-      bassNotes.push(new StaveNote({ keys: ['g/4'], duration: 'hr' }))
-    } else {
-      bassNotes.push(t.sn)
-      trebleNotes.push(new StaveNote({ keys: ['g/4'], duration: 'hr' }))
-    }
+    const s1 = new Stave(10, 40, 200).addClef('treble') //.addTimeSignature('4/4')
+    const s2 = new Stave(10, 110, 200).addClef('bass')
+    const notes = [drawNote(target, s1, s2)]
     if (played) {
-      const t = createNote(played)
-      if (t.clef === 'treble') {
-        trebleNotes.push(t.sn)
-      } else {
-        bassNotes.push(t.sn)
-      }
-    } else {
-      if (trebleNotes.length > 0) {
-        trebleNotes.push(new StaveNote({ keys: ['g/4'], duration: 'hr' }))
-      } else {
-        bassNotes.push(new StaveNote({ keys: ['g/4'], duration: 'hr' }))
-      }
+      notes.push(drawNote(played, s1, s2))
     }
-    Vex.Flow.Formatter.FormatAndDraw(ctx, s1, trebleNotes)
-    Vex.Flow.Formatter.FormatAndDraw(ctx, s2, bassNotes)
+    drawNotesToStaves(s1, s2, notes)
     s1.setContext(ctx).draw()
     s2.setContext(ctx).draw()
     // console.log('draw ', notes)
