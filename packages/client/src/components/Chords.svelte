87d255bd8525ca8701e9ae6a9780f9ffbb98c9e8
Teemu Koivisto <tkoivisto456@gmail.com>
2024-01-28 22:30:23 +0200

    remove unused chords code

--- packages/client/src/components/Chords.svelte
+++ packages/client/src/components/Chords.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
   import { writable } from 'svelte/store'
-  import { chordsMap } from '@/music-scales'
+  import { chords } from '@/music-scales'
 
   import { persist } from '$stores/persist'
 
-  $: chordsList = Array.from(chordsMap.entries())
-  let chordsList2: any[] = []
-  $: {
-    let leftIdx = 0
-    let rightIdx = Math.ceil(chordsList.length / 2) // 15 / 2 = 7 -> 0,6 & 7,14
-    chordsList.forEach((_, idx) => {
-      if (idx % 2 === 0) {
-        chordsList2.push(chordsList[leftIdx++])
-      } else {
-        chordsList2.push(chordsList[rightIdx++])
-      }
-    })
-  }
+  $: chordsList = Array.from(chords.entries())
   $: leftList = chordsList.filter((_, i) => i < chordsList.length / 2)
   $: rightList = chordsList.filter((_, i) => i >= chordsList.length / 2)
-  const hidden = persist(writable(false), { key: 'inputs-hidden' })
+
+  const hidden = persist(writable(false), { key: 'chords-hidden' })
 
   function toggleVisibility() {
     hidden.update(h => !h)
@@ -31,17 +20,6 @@
     <legend class="px-2 text-0A text-base">
       <button class="hover:bg-gray-100" on:click={toggleVisibility}>Chords</button>
     </legend>
-    <!-- <div class="body">
-      {#each chordsList2 as chord}
-        <div class="flex items-center justify-center px-1 bg-gray-200">{chord[0]}</div>
-        <div class="intervals">
-          {#each chord[1].shape as interval}
-            <span>{interval}</span>
-          {/each}
-        </div>
-        <div class="text-xs">{chord[1].name}</div>
-      {/each}
-    </div> -->
     <div class="flex flex-col md:flex-row" class:hidden={$hidden}>
       <ul class="chord-list w-full mr-1 md:w-1/2">
         {#each leftList as chord}
@@ -70,18 +48,8 @@
 </div>
 
 <style lang="scss">
-  .body {
-    display: grid;
-    gap: 0.25rem;
-    grid-template-columns: 1fr 4fr 5fr 1fr 4fr 5fr;
-    grid-template-rows: auto;
-    align-items: center;
-    @media (width <= 700px) {
-      grid-template-columns: 1fr 4fr 5fr;
-    }
-    &.hidden {
-      display: none;
-    }
+  .hidden {
+    display: none;
   }
   .chord-list {
     display: grid;
