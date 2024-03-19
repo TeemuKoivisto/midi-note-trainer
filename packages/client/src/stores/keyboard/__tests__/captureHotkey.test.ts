6f0d05445a5ae0cc39b25268f6a2994c184211fe
Teemu Koivisto <tkoivisto456@gmail.com>
2024-03-19 16:10:36 +0200

    add snapshots to captureHotkey test

--- packages/client/src/stores/keyboard/__tests__/captureHotkey.test.ts
+++ packages/client/src/stores/keyboard/__tests__/captureHotkey.test.ts
@@ -1,7 +1,12 @@
+import { get } from 'svelte/store'
+
 import { capturingHotkeys, keyboardSettings, keyboardActions } from '$stores/keyboard'
 import { inputsActions } from '$stores/inputs'
 import { scoreActions } from '$stores/score'
-import { get } from 'svelte/store'
+
+import capturingHotkeys0 from './__snapshots__/capturing-hotkeys-0.json'
+import customLayout1 from './__snapshots__/custom-layout-1.json'
+import customLayout2 from './__snapshots__/custom-layout-2.json'
 
 describe('captureHotkey', () => {
   beforeAll(() => {
@@ -23,140 +28,17 @@ describe('captureHotkey', () => {
       e: 'guessed-note'
     })
     expect(keyboardActions.handleInput('Tab', 'Tab', false)).toEqual(false)
+    expect(keyboardActions.handleInput('Quote', 'ä', false)).toEqual({
+      data: 77,
+      e: 'guessed-note'
+    })
     expect(get(capturingHotkeys)).toEqual(undefined)
+
+    // Start capturing
     keyboardActions.setCustomLayout(true)
     keyboardActions.captureHotkeyRow(2)
-    expect(get(capturingHotkeys)).toEqual({
-      count: 12,
-      nextIndex: 1,
-      rowIndex: 2,
-      row: [
-        {
-          code: 'Capslock',
-          key: '{lock}'
-        },
-        {
-          code: 'KeyA',
-          key: 'a',
-          note: {
-            flats: 0,
-            note: 'C',
-            semitones: 0,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyS',
-          key: 's',
-          note: {
-            flats: 0,
-            note: 'D',
-            semitones: 2,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyD',
-          key: 'd',
-          note: {
-            flats: 0,
-            note: 'E',
-            semitones: 4,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyF',
-          key: 'f',
-          note: {
-            flats: 0,
-            note: 'F',
-            semitones: 5,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyG',
-          key: 'g',
-          note: {
-            flats: 0,
-            note: 'G',
-            semitones: 7,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyH',
-          key: 'h',
-          note: {
-            flats: 0,
-            note: 'A',
-            semitones: 9,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyJ',
-          key: 'j',
-          note: {
-            flats: 0,
-            note: 'B',
-            semitones: 11,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyK',
-          key: 'k',
-          note: {
-            flats: 0,
-            note: 'C',
-            semitones: 12,
-            sharps: 0
-          }
-        },
-        {
-          code: 'KeyL',
-          key: 'l',
-          note: {
-            flats: 0,
-            note: 'D',
-            semitones: 14,
-            sharps: 0
-          }
-        },
-        {
-          code: 'Semicolon',
-          key: ';',
-          note: {
-            flats: 0,
-            note: 'E',
-            semitones: 16,
-            sharps: 0
-          }
-        },
-        {
-          code: 'Quote',
-          key: "'",
-          note: {
-            flats: 0,
-            note: 'F',
-            semitones: 17,
-            sharps: 0
-          }
-        },
-        {
-          code: 'Backslash',
-          key: '\\',
-          note: {
-            flats: 0,
-            note: 'G',
-            semitones: 19,
-            sharps: 0
-          }
-        }
-      ]
-    })
+    expect(get(capturingHotkeys)).toEqual(capturingHotkeys0)
+    expect(get(capturingHotkeys)?.count).toEqual(12)
     expect(keyboardActions.handleInput('KeyS', 'S', true)).toEqual({
       data: {
         code: 'KeyS',
@@ -195,7 +77,7 @@ describe('captureHotkey', () => {
       e: 'hotkeys-captured-key'
     })
     expect(get(capturingHotkeys)?.nextIndex).toEqual(5)
-    const letters = 'FGHJKLÖÄ'.split('')
+    let letters = 'FGHJKLÖÄ'.split('')
     letters.forEach((letter, idx) => {
       expect(keyboardActions.handleInput(`Key${letter}`, letter, false)).toEqual({
         data: {
@@ -220,119 +102,42 @@ describe('captureHotkey', () => {
       e: 'guessed-note'
     })
     expect(keyboardActions.handleInput('KeyD', 'D', true)).toEqual(false)
-    expect(get(keyboardSettings).customLayout[2]).toEqual([
-      {
-        code: 'Capslock',
-        key: '{lock}'
-      },
-      {
-        code: 'KeyS',
-        key: 'S',
-        note: {
-          flats: 0,
-          note: 'C',
-          semitones: 0,
-          sharps: 0
-        }
-      },
-      {
-        code: 'Tab',
-        key: 'Tab',
-        note: {
-          flats: 0,
-          note: 'D',
-          semitones: 2,
-          sharps: 0
-        }
-      },
-      {
-        code: 'EMPTY',
-        key: '{empty}'
-      },
-      {
-        code: 'EMPTY',
-        key: '{empty}'
-      },
-      {
-        code: 'KeyF',
-        key: 'F',
-        note: {
-          flats: 0,
-          note: 'G',
-          semitones: 7,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyG',
-        key: 'G',
-        note: {
-          flats: 0,
-          note: 'A',
-          semitones: 9,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyH',
-        key: 'H',
-        note: {
-          flats: 0,
-          note: 'B',
-          semitones: 11,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyJ',
-        key: 'J',
-        note: {
-          flats: 0,
-          note: 'C',
-          semitones: 12,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyK',
-        key: 'K',
-        note: {
-          flats: 0,
-          note: 'D',
-          semitones: 14,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyL',
-        key: 'L',
-        note: {
-          flats: 0,
-          note: 'E',
-          semitones: 16,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyÖ',
-        key: 'Ö',
-        note: {
-          flats: 0,
-          note: 'F',
-          semitones: 17,
-          sharps: 0
-        }
-      },
-      {
-        code: 'KeyÄ',
-        key: 'Ä',
-        note: {
-          flats: 0,
-          note: 'G',
-          semitones: 19,
-          sharps: 0
-        }
+    expect(keyboardActions.handleInput('Quote', 'ä', false)).toEqual(false)
+    expect(get(keyboardSettings).customLayout).toEqual(customLayout1)
+    expect(get(capturingHotkeys)).toEqual(undefined)
+
+    // See that reinputting the same row works
+    keyboardActions.captureHotkeyRow(2)
+    expect(get(capturingHotkeys)?.count).toEqual(12)
+    letters = "ASDFGHJKLÖÄ'".split('')
+    letters.forEach((letter, idx) => {
+      expect(keyboardActions.handleInput(`Key${letter}`, letter, false)).toEqual({
+        data: {
+          code: `Key${letter}`,
+          key: letter
+        },
+        e: 'hotkeys-captured-key'
+      })
+      if (idx === letters.length - 1) {
+        expect(get(capturingHotkeys)?.nextIndex).toEqual(undefined)
+      } else {
+        expect(get(capturingHotkeys)?.nextIndex).toEqual(2 + idx)
       }
-    ])
+    })
+    expect(keyboardActions.handleInput('KeyA', 'A', true)).toEqual({
+      data: 72,
+      e: 'guessed-note'
+    })
+    expect(keyboardActions.handleInput('KeyS', 's', false)).toEqual({
+      data: 62,
+      e: 'guessed-note'
+    })
+    expect(keyboardActions.handleInput('Tab', 'Tab', true)).toEqual(false)
+    expect(keyboardActions.handleInput('KeyD', 'D', true)).toEqual({
+      data: 76,
+      e: 'guessed-note'
+    })
+    expect(keyboardActions.handleInput('Quote', 'ä', false)).toEqual(false)
+    expect(get(keyboardSettings).customLayout).toEqual(customLayout2)
   })
 })
